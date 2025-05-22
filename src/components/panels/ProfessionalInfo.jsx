e94c8707b0e7976192f2793d9f2a825a8c57c3eb
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-05-22 13:05:55 +0530

    about me info updated

--- src/components/panels/ProfessionalInfo.jsx
+++ src/components/panels/ProfessionalInfo.jsx
@@ -17,9 +17,8 @@ const ProfessionalInfo = ({ category, h1, h2, h3 }) => {
           case "h1":
             return (
               <MainDashboard
-                number={5}
-                h1="Web Development Intern at Zidio Development"
-                h2="Full Stack Developer at Nuvance Technologies"
+                number={2}
+                h1="Full Stack Developer at Nuvance Technologies"
                 // l1="- Built 2 responsive React.js apps with 100% browser compatibility"
                 // l2="- Improved UI integration and user experience"
                 // l3="- Ensured clean, maintainable code"
