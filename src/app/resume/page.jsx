0a5ede48ff36052f5ab93e43cc0a059bbc0261bc
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-05-28 22:21:19 +0530

    added a file, for quick look, for my convinience

---
+++ src/app/resume/page.jsx
@@ -0,0 +1,7 @@
+export default function Resume() {
+    return (
+        <div className="h-screen">
+            <iframe src="https://drive.google.com/file/d/1v57QaGPrasEJ2GPIfsUARwJyg1hc0RwZ/preview" sandbox="allow-same-origin allow-scripts" className="w-screen h-full"></iframe>
+        </div>
+    );
+}
