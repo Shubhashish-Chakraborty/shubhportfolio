fe12f51c03dc7ed0ebf71d14f861564c618c3d78
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-07-17 18:41:46 +0530

    updated a file

--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -1,7 +1,7 @@
 export default function Resume() {
     return (
         <div className="h-screen">
-            <iframe src="https://drive.google.com/file/d/1v57QaGPrasEJ2GPIfsUARwJyg1hc0RwZ/preview" sandbox="allow-same-origin allow-scripts" className="w-screen h-full"></iframe>
+            <iframe src="https://drive.google.com/file/d/1-l5siTnu7bg8sVQtuaDL2ekqTLSGsQno/preview" sandbox="allow-same-origin allow-scripts" className="w-screen h-full"></iframe>
         </div>
     );
 }
