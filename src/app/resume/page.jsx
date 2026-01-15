d7b40f8c4e747185e50b208755f0610d0ef28cb6
Shubhashish Chakraborty <164302071+Shubhashish-Chakraborty@users.noreply.github.com>
2026-01-15 17:08:15 +0530

    resume updated

--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -1,7 +1,7 @@
 export default function Resume() {
     return (
         <div className="h-screen">
-            <iframe src="https://drive.google.com/file/d/1-l5siTnu7bg8sVQtuaDL2ekqTLSGsQno/preview" sandbox="allow-same-origin allow-scripts" className="w-screen h-full"></iframe>
+            <iframe src="https://drive.google.com/file/d/1h5-D_IiqLpaXxisd68C4d2VLo1mqPsuP/preview" sandbox="allow-same-origin allow-scripts" className="w-screen h-full"></iframe>
         </div>
     );
 }
